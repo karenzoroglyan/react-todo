add604d987e41e81560ba56ea346ef691a81b755
Karen <karen.zoroglyan@mail.ru>
2022-01-03 18:21:03 +0400

    added submit function for TodoForm component

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import TodoList from './components/TodoList';
 
 function App() {
   const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>Todo List</h1>
       </header>
-      <TodoForm setInputText={setInputText}/>
+      <TodoForm todos={todos} setTodods={setTodos} InputText={inputText} setInputText={setInputText}/>
       <TodoList />
     </div>
   );
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,13 +1,24 @@
 import React from "react";
 
-function TodoForm({setInputText}) {
+function TodoForm({inputText, setInputText, todos, setTodos}) {
+
     function inputTextHandler(e) {
         setInputText(e.target.value)
     }
+
+    function submitTodoHandler(e) {
+        e.preventDefault();
+        setTodos([
+            ...todos,
+            {text: inputText, completed: false, id: Math.random() * 1000}
+        ]);
+        setInputText("")
+    }
+
   return (
     <form>
-      <input onChange={inputTextHandler} type="text" className="todo-input" />
-      <button className="todo-button" type="submit">
+      <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
