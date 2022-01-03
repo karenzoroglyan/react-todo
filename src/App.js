e62d82f91332e887b359f97b73428c36b9d28578
Karen <karen.zoroglyan@mail.ru>
2022-01-03 17:05:53 +0400

    created initial state for my app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 //Importing Components
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState('');
   return (
     <div className="App">
       <header>
