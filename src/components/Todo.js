c653ed3bd161108797fe97437fdc8d5ed2cd90b4
Karen <karen.zoroglyan@mail.ru>
2022-01-03 18:38:24 +0400

    added Todo component

---
+++ src/components/Todo.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Todo() {
+  return (
+    <div className="todo">
+      <li className="todo-item"></li>
+      <button className="complete-btn">
+        <i className="fas fa-check"></i>
+      </button>
+      <button className="complete-btn">
+        <i className="fas fa-trash"></i>
+      </button>
+    </div>
+  );
+}
+
+export default Todo;
